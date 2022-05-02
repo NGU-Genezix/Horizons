ba131bbba0b6bc03522afc68a1a43a43dbcb4b74
Dodo <dorian.payet@epitech.eu>
2022-05-02 19:12:37 +0200

    design login/register

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,6 +5,8 @@ import { useHistory } from "react-router-dom";
 import GoogleButtonAuth from "../components/GoogleButtonAuth";
 import LockLogo from "../assets/login-lock-logo.png";
 import Navbar from '../components/Navbar';
+import "../styles/page_login.css";
+<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
  
 const Login = () => {
 
@@ -66,43 +68,56 @@ const Login = () => {
                     <div className="little-circle-2"></div>
                 </div>
                 <div className="login-container">
-                    <h1 className="main-title">Connexion</h1>
+                    <div className="big-square">
+                        <h1 className="main-title">Connexion</h1>
 
-                    <div className="input-1">
-                        <TextFieldAuth
-                            title="email"
-                            onChange={handleChange}
-                            placeholder="Adresse email"
-                            type="text"
-                        />
-                    </div>
-                    <div className="input-2">
-                        <TextFieldAuth
-                            title="password"
-                            onChange={handleChange}
-                            placeholder="Mot de passe"
-                            type="password"
-                        />
-                    </div>
+                        <div className="email-title">Adresse email</div>
+                        <div className="input-1">
+                            <TextFieldAuth
+                                title="email"
+                                onChange={handleChange}
+                                placeholder="Adresse email"
+                                type="text"
+                                style="width: 500px"
+                            />
+                        </div>
+                        <div className="password-title">Mot de passe</div>
+                        <div className="input-2">
+                            <TextFieldAuth
+                                title="password"
+                                onChange={handleChange}
+                                placeholder="Mot de passe"
+                                type="password"
+                            />
+                        </div>
 
-                    <div className="remember-container">
-                        <input type="checkbox" />
-                        <label>Se souvenir de moi</label>
-                    </div>
-                    <div className="separator-auth-methods-container">
-                        <hr></hr>
-                        <span>OU</span>
-                        <hr></hr>
-                    </div>
-                    <GoogleButtonAuth title="Continuer avec Google"/>
-                    <div className="clicable-widgets-container">
-                        <ButtonAuth
-                            onClick={login}
-                            title="Connexion"
-                        />
-                        <div className="link-container">
-                            <a href="#">Mot de passe oublié ?</a>
-                            <a href="#" onClick={() => history.push('/register')}>Je n'ai pas encore de compte</a>
+                        <div className="remember-container">
+                            <input type="checkbox" />
+                            <label>Se souvenir de moi</label>
+                        </div>
+                        <div className="separator-auth-methods-container">
+                            <hr></hr>
+                            <span>OU</span>
+                            <hr></hr>
+                        </div>
+                        <div class="google-btn">
+                            <div class="google-icon-wrapper">
+                                <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+                            </div>
+                            <p class="btn-text"><b>Continuer avec Google</b></p>
+                        </div>
+
+
+
+                        <div className="clicable-widgets-container">
+                            <ButtonAuth
+                                onClick={login}
+                                title="Connexion"
+                            />
+                            <div className="link-container">
+                                <a href="#">Mot de passe oublié?</a>
+                                <a href="#" onClick={() => history.push('/register')}> Je n'ai pas encore de compte</a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/pages/register.js
+++ src/pages/register.js
@@ -7,6 +7,7 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import Checkbox from '@material-ui/core/Checkbox';
 import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import "../styles/page_register.css";
  
 const Register = () => {
 
@@ -59,92 +60,101 @@ const Register = () => {
                     <div className="little-circle-2"></div>
                 </div>
                 <div className="register-container">
-                    <h1 className="main-title">Créer votre compte</h1>
+                    <div className="big-square">
+                        <h1 className="main-title">Créer votre compte</h1>
 
-                    <div className="input-container">
-                        <div className="input-1">
-                            <TextFieldAuth
-                                title="username"
-                                onChange={handleChange}
-                                placeholder="Nom d'utilisateur"
-                                type="text"
-                            />
+                        <div className="input-container">
+                            <div className="one-title">Nom d'utilisateur</div>
+                            <div className="input-1">
+                                <TextFieldAuth
+                                    title="username"
+                                    onChange={handleChange}
+                                    placeholder="Nom d'utilisateur"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="two-title">Prénom</div>
+                            <div className="input-2">
+                                <TextFieldAuth
+                                    title="firstname"
+                                    onChange={handleChange}
+                                    placeholder="Prénom"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="three-title">Nom</div>
+                            <div className="input-3">
+                                <TextFieldAuth
+                                    title="lastname"
+                                    onChange={handleChange}
+                                    placeholder="Nom"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="four-title">Adresse email</div>
+                            <div className="input-4">
+                                <TextFieldAuth
+                                    title="email"
+                                    onChange={handleChange}
+                                    placeholder="Adresse email"
+                                    type="text"
+                                />
+                            </div>
+                            <div className="five-title">Sexe</div>
+                            <div className="input-7">
+                                <label><input 
+                                    type="checkbox" 
+                                />
+                                Homme
+                                </label>
+                                <label><input 
+                                    type="checkbox" 
+                                />
+                                Femme
+                                </label>
+                            </div>
+                            <div className="six-title">Profil</div>
+                            <div className="input-8">
+                                <label><input 
+                                    type="checkbox" 
+                                />
+                                Etudiant
+                                </label>
+                                <label><input 
+                                    type="checkbox" 
+                                />
+                                Personne agée
+                                </label>
+                                <label><input 
+                                    type="checkbox" 
+                                />
+                                Handicap
+                                </label>
+                            </div>
+                            <div className="seven-title">Mot de passe</div>
+                            <div className="input-5">
+                                <TextFieldAuth
+                                    title="password"
+                                    onChange={handleChange}
+                                    placeholder="Mot de passe"
+                                    type="password"
+                                />
+                            </div>
+                            <div className="input-6">
+                                <TextFieldAuth
+                                    title="c_password"
+                                    onChange={handleChange}
+                                    placeholder="Confirmation mot de passe"
+                                    type="password"
+                                />
+                            </div>
                         </div>
-                        <div className="input-2">
-                            <TextFieldAuth
-                                title="firstname"
-                                onChange={handleChange}
-                                placeholder="Prénom"
-                                type="text"
-                            />
-                        </div>
-                        <div className="input-3">
-                            <TextFieldAuth
-                                title="lastname"
-                                onChange={handleChange}
-                                placeholder="Nom"
-                                type="text"
-                            />
-                        </div>
-                        <div className="input-4">
-                            <TextFieldAuth
-                                title="email"
-                                onChange={handleChange}
-                                placeholder="Adresse email"
-                                type="text"
-                            />
-                        </div>
-                        <div className="input-7">
-                            <label><input 
-                                type="checkbox" 
-                            />
-                            Homme
-                            </label>
-                            <label><input 
-                                type="checkbox" 
-                            />
-                            Femme
-                            </label>
-                        </div>
-                        <div className="input-8">
-                            <label><input 
-                                type="checkbox" 
-                            />
-                            Etudiant
-                            </label>
-                            <label><input 
-                                type="checkbox" 
-                            />
-                            Personne agée
-                            </label>
-                            <label><input 
-                                type="checkbox" 
-                            />
-                            Handicap
-                            </label>
-                        </div>
-                        <div className="input-5">
-                            <TextFieldAuth
-                                title="password"
-                                onChange={handleChange}
-                                placeholder="Mot de passe"
-                                type="password"
-                            />
-                        </div>
-                        <div className="input-6">
-                            <TextFieldAuth
-                                title="c_password"
-                                onChange={handleChange}
-                                placeholder="Confirmation mot de passe"
-                                type="password"
-                            />
-                        </div>
-                    </div>
 
-                    <div className="clicable-widgets-container">
-                        <ButtonAuth onClick={register} title="Créer mon compte" />
-                        <div className="link-container">
-                            <a href="#" onClick={() => history.push('/login')}>Vous avez déjà un compte ? Se connecter</a>
+                        <div className="clicable-widgets-container">
+                            <ButtonAuth onClick={register} title="Créer mon compte" />
+                            <div className="link-container">
+                                <a href="#" onClick={() => history.push('/login')}>Vous avez déjà un compte ? Se connecter</a>
+                            </div>
                         </div>
                     </div>
                 </div>
