aa51005d12559b5d1f6fad399ebe6190ca0a3992
Dodo <dorian.payet@epitech.eu>
2022-05-03 07:47:33 +0200

    design datauser

--- src/pages/dataUser.js
+++ src/pages/dataUser.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
+import "../styles/datauser.css";
  
 const DataUser = () => {
 
@@ -15,38 +16,42 @@ const DataUser = () => {
                     <div className="little-circle-2"></div>
                 </div>
                 <div className="register-container">
-                    <h1 className="main-title">PAYET DORIAN</h1>
+                    <div className="big-square">
+                        <h1 className="main-title">PAYET DORIAN</h1>
 
-                    <div className="info-container">
-                        <div className="info-1">
-                            <text>Date de naissance</text>
-                            <br></br>
-                            <text>27/07/2000</text>
+                        <div className="info-container">
+                            <div className="info-1">
+                                <text className="gras">Date de naissance</text>
+                                <br></br>
+                                <text>27/07/2000</text>
+                            </div>
+                            <div className="info-2">
+                                <text className="gras">Adresse mail</text>
+                                <br></br>
+                                <text>dorian.payet@epitech.eu</text>
+                            </div>
+                            <div className="info-3">
+                                <text className="gras">Sexe</text>
+                                <label><input 
+                                    type="checkbox" 
+                                    checked="checked"
+                                />
+                                Homme
+                                </label>
+                            </div>
+                            <div className="info-4">
+                                <text className="gras">Profil</text>
+                                <label><input 
+                                    type="checkbox"
+                                    checked="checked" 
+                                />
+                                Etudiant
+                                </label>
+                            </div>
+                            <div className="info-5">
+                                <button>Modifier</button>
+                            </div>
                         </div>
-                        <div className="info-2">
-                            <text>Adresse mail</text>
-                            <br></br>
-                            <text>dorian.payet@epitech.eu</text>
-                        </div>
-                        <div className="info-3">
-                            <text>Sexe</text>
-                            <label><input 
-                                type="checkbox" 
-                                checked="checked"
-                            />
-                            Homme
-                            </label>
-                        </div>
-                        <div className="info-4">
-                            <text>Profil</text>
-                            <label><input 
-                                type="checkbox"
-                                checked="checked" 
-                            />
-                            Etudiant
-                            </label>
-                        </div>
-                        <button>Modifier</button>
                     </div>
                 </div>
             </div>
