40ed02caa834e354e7d68b0e4c2c2c446780ff0f
NGU-Genezix <thibault.villetard@epitech.eu>
2022-10-14 13:28:59 +0200

    connexion google

--- src/components/GoogleButtonAuth.js
+++ src/components/GoogleButtonAuth.js
@@ -5,10 +5,7 @@ const GoogleButtonAuth = (props) => {
     return (
         <div className="google-button-auth-container" onClick={props.onClick}>
             <div className="google-button-auth-sub-container">
-                <a href="https://gregoriex.fr/connect/google" > HERE</a>
-                <div className="title-container">
-                    <h1 className="google-auth-btn-text">{props.title}</h1>
-                </div>
+                <a href="https://gregoriex.fr/api/connect/google" > HERE</a>
             </div>
         </div>
     );
